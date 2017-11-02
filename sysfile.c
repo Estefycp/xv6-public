5a015405de0d7b9c7bb54fb8ae0c45efaea00eae
estefycp <estefy.cp@hotmail.com>
2017-11-01 21:19:41 -0600

    Add chmod program and syscall

--- sysfile.c
+++ sysfile.c
@@ -375,7 +375,7 @@ sys_chdir(void)
   char *path;
   struct inode *ip;
   struct proc *curproc = myproc();
-  
+
   begin_op();
   if(argstr(0, &path) < 0 || (ip = namei(path)) == 0){
     end_op();
@@ -442,4 +442,22 @@ sys_pipe(void)
   fd[0] = fd0;
   fd[1] = fd1;
   return 0;
+}
+int sys_chmod(void){
+  char *fname, *permissions;
+  struct inode *ip;
+  if(argstr(0, &permissions) < 0 || argstr(1, &fname) < 0)
+    return -1;
+  begin_op();
+  if((ip = namei(fname)) == 0)
+  {
+    end_op();
+    return -1;
+  }
+  ilock(ip);
+  memmove(ip->permissions, permissions, 4);
+  iupdate(ip); // guardar en HDD
+  iunlock(ip);
+  end_op();
+  return 1;
 }
